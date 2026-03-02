797beb5c7472eb00257648a17dafa8b6150abe11
Vitali Zhuravski <vitaliycof@outlook.com>
2026-03-02 17:41:35 +0300

    Для меток теперь гарантируется уникальность.

--- 351003/zhuravski/ArticleHouse/DAO/Models/MarkModel.cs
+++ 351003/zhuravski/ArticleHouse/DAO/Models/MarkModel.cs
@@ -1,8 +1,10 @@
 using System.ComponentModel.DataAnnotations.Schema;
+using Microsoft.EntityFrameworkCore;
 
 namespace ArticleHouse.DAO.Models;
 
 [Table("tbl_mark")]
+[Index(nameof(Name), IsUnique = true)]
 public class MarkModel : Model<MarkModel>
 {
     public string Name {get; set;} = default!;
---
+++ 351003/zhuravski/ArticleHouse/Migrations/20260302143724_UniqueMarks.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ArticleHouse.Migrations
+{
+    /// <inheritdoc />
+    public partial class UniqueMarks : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateIndex(
+                name: "ix_tbl_mark_name",
+                table: "tbl_mark",
+                column: "name",
+                unique: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "ix_tbl_mark_name",
+                table: "tbl_mark");
+        }
+    }
+}
